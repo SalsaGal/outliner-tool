ca2d0c6262311298a57e4ce97aca58c4d8b35b64
SalsaGal <fielddashos@gmail.com>
2022-12-27 03:28:27 +1100

    Live update filters

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod picture;
 use std::path::Path;
 
 use eframe::App;
+use egui::Slider;
 use picture::{Picture, Filter};
 use rfd::FileDialog;
 
@@ -30,6 +31,12 @@ impl ProcessApp {
     fn load_image<P: AsRef<Path>>(&mut self, path: P) {
         self.pictures.push(Picture::new(path, &self.filter).unwrap());
     }
+
+    fn update_filtered(&mut self) {
+        for picture in &mut self.pictures {
+            picture.update(&self.filter);
+        }
+    }
 }
 
 impl App for ProcessApp {
@@ -51,6 +58,15 @@ impl App for ProcessApp {
             }
             if ui.button("Clear images").clicked() {
                 self.pictures.clear();
+            }
+
+            let mut sensitivity = self.filter.sensitivity;
+            ui.label("Sensitivity");
+            ui.add(Slider::new(&mut sensitivity, 0..=255));
+
+            if sensitivity != self.filter.sensitivity {
+                self.filter.sensitivity = sensitivity;
+                self.update_filtered();
             }
         });
         egui::CentralPanel::default().show(ctx, |ui| {
--- src/picture.rs
+++ src/picture.rs
@@ -23,13 +23,21 @@ impl Picture {
         })
     }
 
+    pub fn update(&mut self, filter: &Filter) {
+        let filtered = filter.on_source(&self.source);
+        self.drawn = RetainedImage::from_color_image("", ColorImage::from_rgba_unmultiplied(
+            [filtered.dimensions().0 as usize, filtered.dimensions().1 as usize],
+            filtered.as_flat_samples().as_slice(),
+        ));
+    }
+
     pub fn draw(&self, ui: &mut Ui, ctx: &Context) {
         ui.image(self.drawn.texture_id(ctx), self.drawn.size_vec2());
     }
 }
 
 pub struct Filter {
-    sensitivity: u8,
+    pub sensitivity: u8,
 }
 
 impl Filter {
@@ -48,7 +56,7 @@ impl Filter {
 impl Default for Filter {
     fn default() -> Self {
         Self {
-            sensitivity: u8::MAX / 2,
+            sensitivity: 128,
         }
     }
 }
