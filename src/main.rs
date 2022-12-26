96c76ed5d42766c27dada5a14399de41d270fdc9
SalsaGal <fielddashos@gmail.com>
2022-12-26 21:53:17 +1100

    Start ui

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,51 @@
-use process::Process;
+use std::path::Path;
 
-mod process;
+use eframe::App;
+use image::DynamicImage;
+use rfd::FileDialog;
 
 fn main() {
-    let process = Process::new(image::open("test/test.png").unwrap()).unwrap();
-    process.process().save("test/out.png").unwrap();
+    eframe::run_native(
+        "Outliner",
+        eframe::NativeOptions::default(),
+        Box::new(|_cc| Box::new(ProcessApp::new())),
+    )
+}
+
+struct ProcessApp {
+    images: Vec<DynamicImage>
+}
+
+impl ProcessApp {
+    pub fn new() -> Self {
+        Self {
+            images: Vec::new(),
+        }
+    }
+
+    fn load_image<P: AsRef<Path>>(&mut self, path: P) {}
+}
+
+impl App for ProcessApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        for file in &ctx.input().raw.dropped_files {
+            if let Some(path) = &file.path {
+                self.load_image(path);
+            }
+        }
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Outliner");
+            if ui.button("Open images").clicked() {
+                if let Some(paths) = FileDialog::new().pick_files() {
+                    for path in paths {
+                        self.load_image(path);
+                    }
+                }
+            }
+            if ui.button("Clear images").clicked() {
+                self.images.clear();
+            }
+        });
+    }
 }
